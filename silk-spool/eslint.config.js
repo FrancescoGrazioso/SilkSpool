8e96a448d0df24b59f354040f3a5f2d11816f0d4
Francesco Grazioso <francescograzioso1995@gmail.com>
2025-09-23 19:06:50 +0200

    Aggiunto il plugin Prettier alla configurazione di ESLint per migliorare la formattazione del codice. Rimosso l'import duplicato di Prettier.

--- silk-spool/eslint.config.js
+++ silk-spool/eslint.config.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js';
 import typescript from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import prettier from 'eslint-plugin-prettier';
 
 export default [
   js.configs.recommended,
